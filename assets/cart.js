fa5c9301793acc23c132f416b91fa794f83d6218
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-07-10 20:40:19 +0500

    Commit

--- assets/cart.js
+++ assets/cart.js
@@ -23,7 +23,7 @@ searchBtn.onclick = () => {
   cancelBtn.classList.add("show");
 }
 
-// function creating(){
+// function MakingCart(){
 // var cartTable = document.getElementById('cartTable');
 // var cartMultiplyArray = [];
 // for (let i = 0; i < localStorage.length; i++) {
@@ -39,24 +39,42 @@ searchBtn.onclick = () => {
 //     cartMultiplyArray.push(cartInput.value * ArrayOfProducts[3])
 //   }
 // }
+// }
+// MakingCart()
 
-// var cartInput = document.querySelectorAll('.cartInput')
-// console.log(cartInput);
+
+
+// function UpdatingTotalAmount(){
 // var amount = document.querySelectorAll('.amount')
-// console.log(amount);
-// // console.log(...cartMultiplyArray);
+
+// var amountArray = []
 // for (let i = 0; i < amount.length; i++) {
-//     for (let j = 0; j < cartMultiplyArray.length; j++) {
-//       amount[i].innerText += cartMultiplyArray[j]
-      
-//     }
-  
+//       amountArray.push(amount[i].innerHTML)
 // }
+
+// var totalAmount = 0;
+
+// for (let i = 0; i < amountArray.length; i++) {
+//         amountArray[i] = parseInt(amountArray[i])
+//         totalAmount += amountArray[i];
+// }
+
+// cartTable.innerHTML += `  <tr>
+//             <th style="border-left: 1px solid black;">TOTAL AMOUNT</th>
+//             <th id="totalAmount" style="border-right: 1px solid black;"></th>
+//             <th style="text-align:center;">Delivery Charges : 0 $</th>
+//          </tr>`
+
+
+// var totalAmountTable = document.getElementById('totalAmount')
+// totalAmountTable.innerText = `${totalAmount} $`
 
 // }
 
-// creating()
-function creating(){
+
+// UpdatingTotalAmount()
+
+function MakingCart(){
   var cartTable = document.getElementById('cartTable');
   var cartMultiplyArray = [];
   for (let i = 0; i < localStorage.length; i++) {
@@ -66,54 +84,56 @@ function creating(){
       cartTable.innerHTML += `<tr>
         <td><img src='${ArrayOfProducts[2]}' width='20px' height='20px' alt='Product Image'> ${ArrayOfProducts[0]}</td>
         <td class='amount'>${ArrayOfProducts[3]} $</td>
-        <td align='center'><input type="text" maxlength="2" id='cartInput' class='cartInput' value='1'></td>
+        <td align='center'><input type="text" maxlength="2" class='cartInput' value='1' required></td>
       </tr>`;
-      var cartInput = document.getElementById('cartInput')
-      cartMultiplyArray.push(cartInput.value * ArrayOfProducts[3]); 
     }
   }
-
-  var cartInputs = document.querySelectorAll('.cartInput');
-  var amounts = document.querySelectorAll('.amount');
-
-  cartInputs.forEach((input, index) => {
-    input.addEventListener('input', () => {
-      cartMultiplyArray[index] = input.value * ArrayOfProducts[3];
-      updateAmounts();
-    });
-  });
-
-  function updateAmounts() {
-    amounts.forEach((amount, index) => {
-      amount.innerText = `${cartMultiplyArray[index]} $`;
-    });
-  }
-
-  updateAmounts();
-}
-creating()
-var amount = document.querySelectorAll('.amount')
-var amountArray = []
-for (let i = 0; i < amount.length; i++) {
-      amountArray.push(amount[i].innerHTML)
-}
-
-var totalAmount = 0;
-
-for (let i = 0; i < amountArray.length; i++) {
-        amountArray[i] = parseInt(amountArray[i])
-        totalAmount += amountArray[i];
-}
-
-cartTable.innerHTML += `  <tr>
+  cartTable.innerHTML += `  <tr>
             <th style="border-left: 1px solid black;">TOTAL AMOUNT</th>
             <th id="totalAmount" style="border-right: 1px solid black;"></th>
             <th style="text-align:center;">Delivery Charges : 0 $</th>
-         </tr>`
+         </tr>`;
+  
+  var cartInputs = cartTable.querySelectorAll('.cartInput');
+  for (let i = 0; i < cartInputs.length; i++) {
+    cartInputs[i].addEventListener('input', function() {
+      UpdatingTotalAmount();
+    });
+  }
+}
+MakingCart()
+
+function UpdatingTotalAmount(){
+  var amounts = document.querySelectorAll('.amount')
+  var cartInputs = document.querySelectorAll('.cartInput')
+  var totalAmount = 0;
+  
+  for (let i = 0; i < amounts.length; i++) {
+    totalAmount += parseInt(cartInputs[i].value) * parseInt(amounts[i].innerHTML);
+  }
 
 
-var totalAmountTable = document.getElementById('totalAmount')
-totalAmountTable.innerText = `${totalAmount} $`
+  
+
+  var totalAmountTable = document.getElementById('totalAmount')
+  totalAmountTable.innerText = `${totalAmount} $`
+}
+
+UpdatingTotalAmount()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let icon = document.getElementById('icon')
 let num = 1
